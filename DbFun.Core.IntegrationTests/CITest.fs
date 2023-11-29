700ff0fd5fb0959b254dbb1e15efc76e92ab3e63
jacentino <jacentino@gmail.com>
2023-11-29 11:17:31 +0100

    Testing connection to MS SQL instance from github actions

---
+++ DbFun.Core.IntegrationTests/CITest.fs
@@ -0,0 +1,18 @@
+﻿module CITest
+
+open Xunit
+open System.Configuration
+open Microsoft.Data.SqlClient
+open System
+
+[<Fact>]
+let testConnection() = 
+    let config = ConfigurationManager.OpenExeConfiguration(System.Reflection.Assembly.GetExecutingAssembly().Location)
+    let connectionString = config.ConnectionStrings.ConnectionStrings.["CI_TEST"].ConnectionString
+    use connection = new SqlConnection(connectionString)
+    connection.Open()
+    use command = connection.CreateCommand()
+    command.CommandText <- "select @@version"
+    let ver = command.ExecuteScalar()
+    Console.WriteLine("Version: {0}", ver)
+
